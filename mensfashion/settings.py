f9b87dcb70702861b38d8f429bbcb8a2ba5bcaea
Abdurahiman <abdukallara@gmail.com>
2023-04-16 16:24:16 +0530

    allowedhost

--- mensfashion/settings.py
+++ mensfashion/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'http://mensfashion.abdurahman.online',]
+ALLOWED_HOSTS = ['localhost', 'mensfashion.abdurahman.online',]
 
 
 # Application definition
